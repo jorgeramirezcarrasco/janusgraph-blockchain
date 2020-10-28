2bfa035d881001dcae97bc2b5eb83a489e168e1d
jorgeramirezcarrasco <jorgerc93@msn.com>
2020-10-29 00:14:50 +0100

    feat: updating cliet tsv parsing

--- client/janus_client.py
+++ client/janus_client.py
@@ -8,27 +8,25 @@ import csv
 
 graph = Graph()
 connection = DriverRemoteConnection('ws://localhost:8182/gremlin', 'g')
-# The connection should be closed on shut down to close open connections with connection.close()
+
 g = graph.traversal().withRemote(connection)
-# Reuse 'g' across the application
-print(g)
 
 tsv_file = open('../data/blockchair_ethereum_transactions_20201025.tsv')
 read_tsv = csv.DictReader(tsv_file, delimiter="\t")
 
 for tx in read_tsv:
-  print(tx['hash'])
-  g.addV('tx').property('block_id',tx['block_id']).property('hash',tx['hash']).next()
-  #Check it is stored
+  tx = g.addV('tx')
+  for key in tx.keys:
+    if key == "sender":
+      sender = g.addV('sender').next()
+    if key == "receiver":
+      receiver = g.addV('receiver').next()
+    tx.property(key,tx[key])
+  tx.next()
+  #Check if it is stored
   print(g.V().has('tx','hash',tx['hash']).next())
+  # Create links
+  g.V(Bindings.of('id',tx)).addE('sent by').to(sender).iterate()
+  g.V(Bindings.of('id',tx)).addE('received by').to(receiver).iterate()
 
-'''
-v1 = g.addV('person').property('name','marko').next()
-v2 = g.addV('person').property('name','stephen').next()
-g.V(Bindings.of('id',v1)).addE('knows').to(v2).property('weight',0.75).iterate()
-
-marko = g.V().has('person','name','marko').next()
-print(marko)
-peopleMarkoKnows = g.V().has('person','name','marko').out('knows').toList()
-print(peopleMarkoKnows)
-'''
+connection.close()
